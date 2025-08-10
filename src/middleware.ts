4b6b63b1f30e4593816221a41112805610dc2404
Pawan0603 <www.pawanmthakre@gmail.com>
2025-08-11 00:24:43 +0530

    protecting admin route from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,49 @@
-import { NextResponse, NextRequest } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server';
+import { JWTPayload, jwtVerify } from 'jose'; // Edge runtime ke liye
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  let accessToken = request.cookies.get('accessToken')?.value;
+async function verifyToken(token: string): Promise<JWTPayload | null> {
+  try {
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET); // Edge-compatible encoding
+    const { payload } = await jwtVerify(token, secret);
+    return payload;
+  } catch (err) {
+    return null;
+  }
+}
 
+export async function middleware(request: NextRequest) {
+  const accessToken = request.cookies.get('accessToken')?.value;
+  
+  const decodedToken = accessToken ? await verifyToken(accessToken) : null;
+  const userRole = decodedToken?.role || 'customer';
+
+  console.log('Access Token:', accessToken);
+  console.log('User Role:', userRole);
+
+  // protect login and signup routes for authenticated users
   if (accessToken && (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/signup'))) {
-    // If access token exists and user is trying to access login or signup page, redirect to home
     return NextResponse.redirect(new URL('/', request.url));
   }
 
-  if (!accessToken && (request.nextUrl.pathname.startsWith('/profile'))) {
-    // If access token does not exist and user is trying to access profile, redirect to login
+  // Profile route protection
+  if (!accessToken && request.nextUrl.pathname.startsWith('/profile')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  
-
+  // Admin route protection
+  if ((!accessToken || userRole !== 'admin') && request.nextUrl.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
 }
 
 export const config = {
-  matcher: ['/login', '/signup', '/forgot-password', '/reset-password', '/verify-email', '/profile'],
-}
+  matcher: [
+    '/login',
+    '/signup',
+    '/forgot-password',
+    '/reset-password',
+    '/verify-email',
+    '/profile',
+    '/admin/:path*'
+  ],
+};
